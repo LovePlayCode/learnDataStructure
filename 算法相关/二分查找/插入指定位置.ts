4e8a563a1320d0fbd25aa99f10f0f75e4abeae67
lhl <1244224501@qq.com>
2024-10-03 11:05:56 +0800

    feat: 插入指定位置

--- 算法相关/二分查找/插入指定位置.ts
+++ 算法相关/二分查找/插入指定位置.ts
@@ -5,6 +5,14 @@
 给定一个长度为 𝑛 的有序数组 nums 和一个元素 target ，数组不存在重复元素。现将 target 插入数 组 nums 中，
 并保持其有序性。若数组中已存在元素 target ，则插入到其左方。请返回插入后 target 在数组中的索引。
  */
+
+
+/**
+ * 不存在重复元素
+ * @param nums 
+ * @param target 
+ * @returns 
+ */
 function binarySearchInsertionSimple(nums: number[], target: number) {
     let leftIndex = 0
     let rightIndex = nums.length-1
@@ -16,14 +24,39 @@ function binarySearchInsertionSimple(nums: number[], target: number) {
             return
         }
         if(nums[m] < target){
-            rightIndex = m-1
-        }else{
             leftIndex = m+1
+           
+        }else{
+            rightIndex = m-1
         }
     }
     // return leftIndex
     nums.splice(leftIndex,0,target)
 }
-const nums = [1,2,3,4]
-binarySearchInsertionSimple(nums,2)
-console.log(nums)
+const nums = [1,2,2,3,4]
+binarySearchInsertionSimple(nums,0)
+
+/**
+ * 解法 2  有重复元素的存在
+ */
+
+function binarySearchInsertion(nums: number[],target: number){
+    let i = 0
+    let j = nums.length-1
+    while(i<=j){
+        const mid = Math.floor(i+(j-i)/2)
+        if(nums[mid] < target){
+            i = mid+1
+        }else if(nums[mid] > target){
+            j = mid - 1
+        }else{
+            j = j-1
+            if(nums[j] === target){
+                return j
+            }
+        }
+    }
+    return i
+}
+
+console.log('数据为',binarySearchInsertion([1, 2, 2,2, 3,3, 4], 3));
