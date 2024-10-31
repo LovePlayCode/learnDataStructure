d1644a2a9764572a998261da9c5a17d570624530
yyxwan1314 <1244224501@qq.com>
2024-10-31 20:39:24 +0800

    feat: 面试题

--- 算法相关/面试现场code/index2.js
+++ 算法相关/面试现场code/index2.js
@@ -9,6 +9,8 @@
 
  */
 
+const input = "3[a2[cds3[j]]]";
+// cdscdsjjj
 /**
  *
  * @param {string} input
@@ -17,40 +19,52 @@
  * // 学习途径: 掘金 极客。
  */
 function decodeString(input) {
-  //
   const stack = [];
-
   const num = [];
-  for (let i = 0; i < input.length; i++) {
-    const char = input[i];
-    if (char === stack[stack.length]) {
-    }
-
+  const str = [];
+  let curStr = "";
+  let curNum = 0;
+  for (let char of input) {
     if (char === "[") {
-      stack.push("]");
+      num.push(parseInt(curNum));
+      str.push(curStr);
+      curStr = "";
+      curNum = 0;
+    } else if (char === "]") {
+      const reNum = num.pop();
+      const reCurStr = str.pop();
+      curStr = reCurStr + curStr.repeat(reNum);
+      // str.push(formatStr);
+    } else if (/\d/.test(char)) {
+      curNum = parseInt(char);
+    } else {
+      curStr = curStr + char;
     }
-    const textNum = /a-z/;
   }
+  // console.log(str);
+  return curStr;
 }
 
-function app() {
-  setTimeout(() => {
-    console.log("1-1");
-    Promise.resolve().then(() => {
-      console.log("2-1");
-    });
-  });
+const str = decodeString(input);
+console.log(str);
+// function app() {
+//   setTimeout(() => {
+//     console.log("1-1");
+//     Promise.resolve().then(() => {
+//       console.log("2-1");
+//     });
+//   });
 
-  console.log("1-2");
+//   console.log("1-2");
 
-  Promise.resolve().then(() => {
-    console.log("1-3");
-    setTimeout(() => {
-      console.log("3-1");
-    });
-  });
-}
-app();
+//   Promise.resolve().then(() => {
+//     console.log("1-3");
+//     setTimeout(() => {
+//       console.log("3-1");
+//     });
+//   });
+// }
+// app();
 // 1-2
 // 1-3
 // 1-1
