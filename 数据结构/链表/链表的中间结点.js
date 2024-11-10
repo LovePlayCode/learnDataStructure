e7466b5ec154c4bcae662513cd25b26464c79593
lhl <1244224501@qq.com>
2024-11-10 11:35:41 +0800

    map 解中间节点

---
+++ 数据结构/链表/链表的中间结点.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+    const map = new Map()
+    let count = 0
+    while(head){
+        map.set(count,head)
+        head = head.next
+        count++
+    }
+    const mid = Math.floor(count / 2)
+    return map.has(mid)
+};
