bcc8453520019be50f9ef6417c183f65a4e9bd9a
lhl <1244224501@qq.com>
2024-11-17 11:05:50 +0800

    feat: 二分查找相关例子

---
+++ 算法相关/二分查找/查找第一个大于等于给定值的元素.js
@@ -0,0 +1,26 @@
+function bsearch(nums, val) {
+  const n = nums.length;
+  let left = 0;
+  let right = n - 1;
+  while (left <= right) {
+    const mid = left + Math.floor((right - left) / 2);
+    // console.log(mid);
+    if (nums[mid] <= val) {
+      left = mid + 1;
+    }
+    if (nums[mid] > val) {
+      if (mid === 0) {
+        return mid;
+      }
+      if (nums[mid - 1] <= val) {
+        return mid;
+      }
+      right = mid - 1;
+    }
+  }
+  return -1;
+}
+
+const arr = [1, 3, 4, 5, 6, 8, 8, 8, 11, 18];
+const resData = bsearch(arr, 3);
+console.log(resData);
