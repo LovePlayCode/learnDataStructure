4669153f3418b17c76b6e768966567a8623c45c6
lhl <1244224501@qq.com>
2024-11-17 20:46:40 +0800

    feat: 冒泡排序

--- 算法相关/排序/冒泡排序.js
+++ 算法相关/排序/冒泡排序.js
@@ -1,24 +1,21 @@
-
 /**
- * 
- * @param {number[]} nums 
+ *
+ * @param {number[]} nums
  */
-function bubblingSort(nums){
-    if(nums.length < 1){
-        return 
+function bubblingSort(nums) {
+  if (nums.length < 1) {
+    return;
+  }
+  const n = nums.length;
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n - i - 1; j++) {
+      if (nums[j] > nums[j + 1]) {
+        // console.log(nums[j],nums[j+1]);
+        [nums[j], nums[j + 1]] = [nums[j + 1], nums[j]];
+      }
     }
-    const n = nums.length
-    for(let i=0;i<n;i++){
-        for(let j=0;j<n-i-1;j++){
-            if(nums[j] > nums[j+1]){
-                // console.log(nums[j],nums[j+1]);
-                [nums[j],nums[j+1]] = [nums[j+1],nums[j]]
-                console.log(nums[j],nums[j+1])
-            }
-        }
-    }
-    console.log(nums);
-    
+  }
+  console.log(nums);
 }
 
-bubblingSort([3,5,4,1,2,6])
+bubblingSort([3, 5, 4, 1, 2, 6]);
