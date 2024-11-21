2865e83f0e9273ba21df492c27fb043a413c78bd
lhl <1244224501@qq.com>
2024-11-21 09:00:24 +0800

    feat: 全排列

--- 算法相关/递归/全排列.js
+++ 算法相关/递归/全排列.js
@@ -18,3 +18,15 @@ function printPermutations(data, length, k) {
 }
 
 printPermutations([1, 2, 3], 3, 3);
+
+function cellCount(n) {
+  // 基础条件：小时数为 0 或小于 0 时，只有 1 个初始细胞
+  if (n <= 0) {
+    return 1;
+  }
+  // 递归关系：当前细胞数 = 前 1 小时细胞数 + 前 2 小时细胞数 + 前 3 小时细胞数
+  return cellCount(n - 1) + cellCount(n - 2) + cellCount(n - 3);
+}
+
+// 测试例子
+console.log(cellCount(3)); // 输出容器中 5 小时后的细胞数量
