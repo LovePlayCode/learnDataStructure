3d268ed91eb1be600eb85aaad7876f026ffc9880
lhl <1244224501@qq.com>
2024-12-05 08:57:47 +0800

    feat: 好后缀规则

--- 算法相关/BM算法(用于匹配主串和模式串/index.js
+++ 算法相关/BM算法(用于匹配主串和模式串/index.js
@@ -16,6 +16,48 @@ function generateBC(b, m) {
 }
 
 /**
+ *
+ * @param {string[]} b  表示模式串
+ * @param {number} m 表示长度
+ * @param {*} suffix 后缀数组
+ * @param {*} prefix 前缀数组
+ * 1. 初始化suffix和prefix数组
+ * 2. 遍历子串长度，在遍历内部从后往前迭代，填充suffix, 判断条件为当前的b[j]的字符 == b[m-1-k]的字符
+ * 3. 如果 j == -1 说明公共后缀子串也是模式串的前缀子串
+ */
+function generateGS(b, m) {
+  const suffix = new Array(m).fill(-1);
+  const prefix = new Array(m).fill(false);
+
+  for (let i = 0; i < m - 1; i++) {
+    let j = i;
+    let k = 0;
+    while (j >= 0 && b[j] === b[m - 1 - k]) {
+      --j;
+      ++k;
+      suffix[k] = j + 1;
+    }
+    if (j === -1) {
+      prefix[k] = true;
+    }
+  }
+  return [suffix, prefix];
+}
+
+function moveByGS(j, m, suffix, prefix) {
+  const k = m - 1 - j;
+  if (suffix[k] !== -1) {
+    return j - suffix[k] + 1;
+  }
+  for (let r = j + 2; r <= m - 1; r++) {
+    if (prefix[m - r] === true) {
+      return r;
+    }
+  }
+  return m;
+}
+
+/**
  * BM算法，用于匹配模式串在主串中出现的位置
  * @param {string[]} a 主串
  * @param {*} n 主串长度
@@ -31,6 +73,8 @@ function bm(a, n, b, m) {
    * 5. 否则进行移动，这里用 主串进行移动
    */
   const bc = generateBC(b, m);
+  const [suffix, prefix] = generateGS(b, m);
+
   let i = 0;
   while (i <= n - m) {
     for (let j = m - 1; j >= 0; j--) {
@@ -41,6 +85,15 @@ function bm(a, n, b, m) {
     if (j < 0) {
       return i;
     }
-    i = i + (j - bc[a[i + j]]);
+
+    // a[i + j]： 找到的是在主串中坏字符是什么  然后通过哈希表找到在模式串中的位置。
+    const x = j - bc[a[i + j]];
+    let y = 0;
+
+    // 利用好后缀规则匹配
+    if (j < 0) {
+      y = moveByGS(j, m, suffix, prefix);
+    }
+    i = i + Math.max(x, y);
   }
 }
