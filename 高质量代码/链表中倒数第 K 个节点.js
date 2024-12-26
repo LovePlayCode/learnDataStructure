840f20fa08c186801e6833dddf92c4c908737c74
lhl <1244224501@qq.com>
2024-12-26 08:53:07 +0800

    feat: 栈的解法

---
+++ 高质量代码/链表中倒数第 K 个节点.js
@@ -0,0 +1,27 @@
+class ListNode {
+  val = "";
+  next = null;
+  constructor(val, next) {
+    this.val = val;
+    this.next = next;
+  }
+}
+
+/**
+ * @param {ListNode} head
+ * @param {number} num
+ */
+function findListNodeCountBackwardsNode(head, num) {
+  const stack = [];
+  while (head !== null && head !== undefined) {
+    stack.unshift(head);
+    head = head.next;
+  }
+  return stack[num - 1];
+}
+const listNode = new ListNode(0);
+listNode.next = new ListNode(100);
+listNode.next.next = new ListNode(300);
+listNode.next.next.next = new ListNode(20);
+const node = findListNodeCountBackwardsNode(listNode, 3);
+console.log(node);
