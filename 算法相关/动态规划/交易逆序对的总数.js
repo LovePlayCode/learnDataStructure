c8cf27b72764e7b9ba2ae446d8aaaf5acac037a5
lhl <1244224501@qq.com>
2025-01-21 08:37:17 +0800

    feat: 计算逆序对

---
+++ 算法相关/动态规划/交易逆序对的总数.js
@@ -0,0 +1,13 @@
+var reversePairs = function (record) {
+  mergeNumber(record, 0, record.length);
+};
+
+/**
+ * 归并排序
+ * @param {number[]} nums
+ * @param {number} left
+ * @param {number} right
+ */
+function mergeNumber(nums, left, right) {
+    if(){}
+}
