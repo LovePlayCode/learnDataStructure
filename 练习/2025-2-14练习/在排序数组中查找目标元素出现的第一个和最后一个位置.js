b5351b73224ad0c301c08b3ad3908424a84b271b
lhl <1244224501@qq.com>
2025-02-14 08:40:27 +0800

    feat: 排序数组中查找目标元素

---
+++ 练习/2025-2-14练习/在排序数组中查找目标元素出现的第一个和最后一个位置.js
@@ -0,0 +1,55 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function (nums, target) {
+  if (!Array.isArray(nums)) {
+    throw new TypeError("类型错误~");
+  }
+  const first = FirstIndex(nums, target, 0, nums.length - 1);
+  const last = GetNumberOfLast(nums, target, 0, nums.length - 1);
+  return [first, last];
+};
+
+function FirstIndex(nums, target, left, right) {
+  if (left > right) {
+    return -1;
+  }
+  const mid = (left + right) >> 1;
+  if (nums[mid] === target) {
+    if (mid === 0 || (mid > 0 && nums[mid - 1] !== target)) {
+      return mid;
+    } else {
+      right = mid - 1;
+    }
+  } else if (nums[mid] > target) {
+    right = mid - 1;
+  } else {
+    left = mid + 1;
+  }
+  return FirstIndex(nums, target, left, right);
+}
+
+function GetNumberOfLast(nums, k, left, right) {
+  if (left > right) {
+    return -1;
+  }
+  const mid = (left + right) >> 1;
+  const curVal = nums[mid];
+  if (curVal === k) {
+    if (
+      (mid < nums.length - 1 && nums[mid + 1] !== k) ||
+      mid === nums.length - 1
+    ) {
+      return mid;
+    } else {
+      left = mid + 1;
+    }
+  } else if (curVal > k) {
+    right = mid - 1;
+  } else {
+    left = left + 1;
+  }
+  return GetNumberOfLast(nums, k, left, right);
+}
