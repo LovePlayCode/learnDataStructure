4d3378fecb70dc8b37c9f1465aa10728c9c36e58
xiaolong <51304857+LovePlayCode@users.noreply.github.com>
2025-02-19 07:49:39 +0000

    feat: 替换后的最长重复字符

---
+++ 练习/2025-2-19练习/替换后的最长重复字符.ts
@@ -0,0 +1,11 @@
+
+/**
+ * 
+ * 替换后的最长重复字符
+ * leetcode: https://leetcode.cn/problems/longest-repeating-character-replacement/
+ */
+function characterReplacement(s: string, k: number): number {
+    
+};
+
+export {}
