15c2233ec9c92635be46da75a7c3e02174dbc771
xiaolong <51304857+LovePlayCode@users.noreply.github.com>
2025-02-20 13:28:51 +0000

    feat: 子集相关

--- 算法相关/回溯算法/子集.ts
+++ 算法相关/回溯算法/子集.ts
@@ -19,4 +19,23 @@ function subsets(nums: number[]): number[][] {
     return res
 };
 
+// 思路 2，一层一层执行
+function subsets2(nums: number[]): number[][] {
+    const len = nums.length
+    const res: number[][] = [[]]
+    const dfs = (start: number,state: number[],count: number)=>{
+        if(state.length === count){
+            res.push([...state])
+        }
+        for(let i = start;i < len; i++){
+            state.push(nums[i])
+            dfs(i+1,state,count)
+            state.pop()
+        }
+    }
+    for(let i = 1; i <= len; i++){
+        dfs(0,[],i)
+    }
+    return res
+};
 export {}
