f281eb42691e7ea36c88b753521bec4f1507bf38
lhl <1244224501@qq.com>
2025-02-20 21:42:01 +0800

    feat: 子集相关

--- 算法相关/回溯算法/子集.ts
+++ 算法相关/回溯算法/子集.ts
@@ -1,42 +1,41 @@
-
 /**
- * 
+ *
  * 子集
  * leetcode: https://leetcode.cn/problems/subsets/
  */
 function subsets(nums: number[]): number[][] {
-    const len = nums.length
-    const res: number[][] = []
-    const dfs = (start,subset)=>{
-        res.push([...subset])
-        for(let i = start; i < len; i++){
-            subset.push(nums[i])
-            dfs(i+1,subset)
-            subset.pop() 
-        }
+  const len = nums.length;
+  const res: number[][] = [];
+  const dfs = (start, subset) => {
+    res.push([...subset]);
+    for (let i = start; i < len; i++) {
+      subset.push(nums[i]);
+      dfs(i + 1, subset);
+      subset.pop();
     }
-    dfs(0,[])
-    return res
-};
+  };
+  dfs(0, []);
+  return res;
+}
 
 // 思路 2，一层一层执行
 function subsets2(nums: number[]): number[][] {
-    const len = nums.length
-    const res: number[][] = [[]]
-    const dfs = (start: number,state: number[],count: number)=>{
-        if(state.length === count){
-            res.push([...state])
-            return
-        }
-        for(let i = start;i < len; i++){
-            state.push(nums[i])
-            dfs(i+1,state,count)
-            state.pop()
-        }
+  const len = nums.length;
+  const res: number[][] = [[]];
+  const dfs = (start: number, state: number[], count: number) => {
+    if (state.length === count) {
+      res.push([...state]);
+      return;
     }
-    for(let i = 1; i <= len; i++){
-        dfs(0,[],i)
+    for (let i = start; i < len; i++) {
+      state.push(nums[i]);
+      dfs(i + 1, state, count);
+      state.pop();
     }
-    return res
-};
-export {}
+  };
+  for (let i = 1; i <= len; i++) {
+    dfs(0, [], i);
+  }
+  return res;
+}
+export {};
