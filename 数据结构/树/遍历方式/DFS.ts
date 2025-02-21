e2238a6df8a3f7cb011f31cb4310ec51cc9b6f09
lhl <1244224501@qq.com>
2025-02-21 19:41:27 +0800

    feat: DFS

--- 数据结构/树/遍历方式/DFS.ts
+++ 数据结构/树/遍历方式/DFS.ts
@@ -1,36 +1,35 @@
 /**
  * 前序遍历
  */
-function preOrder(root,list){
-    // const list = []
-    if(root === null){
-        return null
-    }
-    list.push(root.val) 
+function preOrder(root, list) {
+  // const list = []
+  if (root === null) {
+    return null;
+  }
+  list.push(root.val);
 
-    preOrder(root.left,list)
-    preOrder(root.right,list)
-
+  preOrder(root.left, list);
+  preOrder(root.right, list);
 }
 /**
  * 中序遍历
  */
-function inOrder(root,list){
-    if(root === null ){
-        return null
-    }
-    inOrder(root.left,list)
-    list.push(root.val)
-    inOrder(root.right,list)
+function inOrder(root, list) {
+  if (root === null) {
+    return null;
+  }
+  inOrder(root.left, list);
+  list.push(root.val);
+  inOrder(root.right, list);
 }
 /**
  * 后序遍历
  */
-function postOrder(root,list){
-    if(root === null){
-        return null
-    }
-    postOrder(root.left,list)
-    postOrder(root.right,list)
-    list.push(root.val)
+function postOrder(root, list) {
+  if (root === null) {
+    return null;
+  }
+  postOrder(root.left, list);
+  postOrder(root.right, list);
+  list.push(root.val);
 }
