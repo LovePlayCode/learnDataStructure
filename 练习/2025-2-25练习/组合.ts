56b4018bcfde8eb12bfc673d25e22e8463f07335
lhl <1244224501@qq.com>
2025-02-25 08:43:34 +0800

    feat: 组合

---
+++ 练习/2025-2-25练习/组合.ts
@@ -0,0 +1,19 @@
+function combine(n: number, k: number): number[][] {
+  const res: number[][] = [];
+
+  const dfs = (start: number, state: number[]) => {
+    if (state.length === k) {
+      res.push([...state]);
+      return;
+    }
+    for (let i = start; i <= n; i++) {
+      state.push(i);
+      dfs(i + 1, state);
+      state.pop();
+    }
+  };
+  dfs(1, []);
+  return res;
+}
+
+export {};
