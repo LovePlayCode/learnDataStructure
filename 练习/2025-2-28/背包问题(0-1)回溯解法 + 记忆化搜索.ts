1189fb4f1f068c3b8357edb747ab44ea58dea25f
xiaolong <51304857+LovePlayCode@users.noreply.github.com>
2025-02-28 08:46:58 +0000

    feat: 记忆化搜索

--- 练习/2025-2-28/背包问题(0-1)回溯解法 + 记忆化搜索.ts
+++ 练习/2025-2-28/背包问题(0-1)回溯解法 + 记忆化搜索.ts
@@ -1,7 +1,8 @@
-/* 0-1 背包：暴力搜索 */
-function knapsackDFS(
+/* 0-1 背包：记忆化搜索 */
+function knapsackDFSMem(
     wgt: Array<number>,
     val: Array<number>,
+    mem: Array<Array<number>>,
     i: number,
     c: number
 ): number {
@@ -9,14 +10,19 @@ function knapsackDFS(
     if (i === 0 || c === 0) {
         return 0;
     }
+    // 若已有记录，则直接返回
+    if (mem[i][c] !== -1) {
+        return mem[i][c];
+    }
     // 若超过背包容量，则只能选择不放入背包
     if (wgt[i - 1] > c) {
-        return knapsackDFS(wgt, val, i - 1, c);
+        return knapsackDFSMem(wgt, val, mem, i - 1, c);
     }
-    // 计算不放入和放入物品 i 的最大价值 决策完 i 轮，剩下的是 i-1 轮的决策
-    const no = knapsackDFS(wgt, val, i - 1, c);
-    const yes = knapsackDFS(wgt, val, i - 1, c - wgt[i - 1]) + val[i - 1];
-    // 返回两种方案中价值更大的那一个
-    return Math.max(no, yes);
+    // 计算不放入和放入物品 i 的最大价值
+    const no = knapsackDFSMem(wgt, val, mem, i - 1, c);
+    const yes =
+        knapsackDFSMem(wgt, val, mem, i - 1, c - wgt[i - 1]) + val[i - 1];
+    // 记录并返回两种方案中价值更大的那一个
+    mem[i][c] = Math.max(no, yes);
+    return mem[i][c];
 }
-export {}
