8b805dcc3400e91910750d4f0ab6483ec8515c7c
xiaolong <51304857+LovePlayCode@users.noreply.github.com>
2025-03-03 09:02:22 +0000

    feat: 子集

--- 算法相关/回溯算法/子集(选或不选).ts
+++ 算法相关/回溯算法/子集(选或不选).ts
@@ -1,5 +1,8 @@
 /**
  * 选或不选的解法
+ * 进行子集的操作的时候，总共有两种操作:
+ * 1. 选择当前元素，继续
+ * 2. 不选当前元素，继续。 所以需要选择或不选
  * 建议看看 liweiwei 题解: https://www.bilibili.com/video/BV147411A7Yq/?p=2
  */
 function subsets(nums: number[]): number[][] {
@@ -10,7 +13,9 @@ function subsets(nums: number[]): number[][] {
       res.push([...stack]);
       return;
     }
+    // 不选
     dfs(index + 1, stack);
+    // 选
     stack.push(nums[index]);
     dfs(index + 1, stack);
     stack.pop();
