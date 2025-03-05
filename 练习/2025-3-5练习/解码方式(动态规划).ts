18c6a9557cf65ad9880c5283f43aa782655a7744
xiaolong <51304857+LovePlayCode@users.noreply.github.com>
2025-03-05 10:01:56 +0000

    feat: 解码方式动态规划写法

---
+++ 练习/2025-3-5练习/解码方式(动态规划).ts
@@ -0,0 +1,24 @@
+function numDecodings(s: string): number {
+    if (s.length === 0 || s[0] === '0') {
+        return 0;
+    }
+    const len = s.length
+    const dp = new Array(len+1).fill(0)
+    dp[0] = 1
+    dp[1] = 1
+    // i-1 才是当前处理的字符
+    for(let i = 2; i <=len; i++){
+        const oneDigit = parseInt(s.substring(i - 1, i));
+        const twoDigits = parseInt(s.substring(i - 2, i));
+        // 当前的值由 i-1 和 i组成，所以最大值由 i-1 决定
+        if (oneDigit >= 1 && oneDigit <= 9) {
+            dp[i] += dp[i - 1];
+        }
+        if (twoDigits >= 10 && oneDigit <= 26) {
+            dp[i] += dp[i - 2];
+        }
+
+    }
+    return dp[len]
+};
+export {}
