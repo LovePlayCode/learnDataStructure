5978be9298918fe15684d9726f659ddfd57ca342
xiaolong <51304857+LovePlayCode@users.noreply.github.com>
2025-03-10 12:32:13 +0000

    feat: 买卖股票

--- 算法相关/动态规划/买卖股票.ts
+++ 算法相关/动态规划/买卖股票.ts
@@ -1,9 +1,14 @@
+
+/**
+1. 动态规划用于求解 "多阶段决策问题"
+2. 动态规划问题的问法：只问最优解，不问具体的解。
+3. 无后效性：把约束条件设置成状态。 比如买卖股票。
+ */
+
 function maxProfit(prices: number[]): number {
     const len = prices.length
     /**
-     * 
-     *  dp[i][j]：下标为 i 这一天结束的时候，手上持股状态为 j 时，我们持有的现金数。换种说法：dp[i][j] 表示天数 [0, i] 区间里，下标 i 这一天状态为 j 的时候能够获得的最大利润。其中：
-
+     * dp[i][j]：下标为 i 这一天结束的时候，手上持股状态为 j 时，我们持有的现金数。换种说法：dp[i][j] 表示天数  [0, i] 区间里，下标 i 这一天状态为 j 的时候能够获得的最大利润。其中：
         作者：liweiwei1419
         链接：https://leetcode.cn/problems/best-time-to-buy-and-sell-stock/solutions/38477/bao-li-mei-ju-dong-tai-gui-hua-chai-fen-si-xiang-b/
         来源：力扣（LeetCode）
@@ -13,11 +18,12 @@ function maxProfit(prices: number[]): number {
         return new Array(2).fill(0)
     })
     dp[0][0] = 0
+    // dp[0][1] 表示第一天买入持股，如果是持股，只能是-的。
     dp[0][1] = -prices[0]
     for(let i = 1; i < len; i++){
         // 今天不持股，那么状态由昨天不持股，以及昨天持股，今天卖了转移而来
         dp[i][0] = Math.max(dp[i-1][0],dp[i-1][1] + prices[i])
-        // 今天持股,昨天也持股，相当于什么都没做。 昨天没持股，今天买入，最大值就是负数
+        // 今天持股,昨天也持股，相当于什么都没做。 昨天没持股，相当于是 0，今天买入，所以直接为负数。
         dp[i][1] = Math.max(dp[i-1][1],-prices[i])
     }
     return dp[len-1][0]
