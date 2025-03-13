5fce79ca7bdc22c831691d4875fb04c55da2b6e3
lhl <1244224501@qq.com>
2025-03-13 19:53:20 +0800

    feat: 买卖股票

--- 算法相关/动态规划/买卖股票2(滚动变量优化).ts
+++ 算法相关/动态规划/买卖股票2(滚动变量优化).ts
@@ -1,22 +1,22 @@
 function maxProfit(prices: number[]): number {
-    const len = prices.length
-    
-    // 不持股情况，利润为 0
-    let cash = 0
+  const len = prices.length;
 
-    // 持股情况，利润为-
-    let hold = -prices[0]
-    let preCash = cash
-    let preHold = hold
-    for(let i = 1; i < len; i++){
-        // 今天不持股，状态由昨天不持股，以及昨天持股，今天售卖得到
-        cash = Math.max(preCash,preHold+prices[i])
-        // 今天持股
-        hold = Math.max(preHold,preCash - prices[i])
-        preCash = cash
-        preHold = hold
-    }
+  // 不持股情况，利润为 0
+  let cash = 0;
 
-    return cash
-};
-export {}
+  // 持股情况，利润为- 变量的含义是记录当前的利润，代表的只是持股的利润和不持股的利润。
+  let hold = -prices[0];
+  let preCash = cash;
+  let preHold = hold;
+  for (let i = 1; i < len; i++) {
+    // 今天不持股，状态由昨天不持股，以及昨天持股，今天售卖得到
+    cash = Math.max(preCash, preHold + prices[i]);
+    // 今天持股
+    hold = Math.max(preHold, preCash - prices[i]);
+    preCash = cash;
+    preHold = hold;
+  }
+
+  return cash;
+}
+export {};
