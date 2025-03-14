07c4a5e5fae4196d639ba1938a318180dc304244
lhl <1244224501@qq.com>
2025-03-14 17:46:03 +0800

    feat: 买卖股票 3

---
+++ 算法相关/动态规划/买卖股票3(回溯+记忆化).ts
@@ -0,0 +1,31 @@
+function maxProfit(prices: number[]): number {
+  const len = prices.length;
+  const memo = new Map(); // 记忆化搜索
+  const dfs = (index: number, transactions: number, holding: number) => {
+    if (index >= len || transactions >= 2) {
+      return 0;
+    }
+    const key = `${index}-${transactions}-${holding}`;
+    if (memo.has(key)) return memo.get(key);
+    // 什么都不干，直接往下走
+    let maxPrice = dfs(index + 1, transactions, holding) || 0;
+    // 如果当前持有股票
+    if (holding) {
+      maxPrice = Math.max(
+        maxPrice,
+        prices[index] + dfs(index + 1, transactions + 1, 0)
+      );
+    } else {
+      // 当前没持有股票
+      maxPrice = Math.max(
+        maxPrice,
+        -prices[index] + dfs(index + 1, transactions, 1)
+      );
+    }
+    memo.set(key, maxPrice);
+    return maxPrice;
+  };
+  return dfs(0, 0, 0);
+}
+
+export {};
