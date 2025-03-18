4c1aa1d75e0f188199c58e3b63f49f204e4f27fa
lhl <1244224501@qq.com>
2025-03-18 17:48:16 +0800

    最长递增子序列

---
+++ 算法相关/动态规划/最长递增子序列.ts
@@ -0,0 +1,20 @@
+/**
+ * @param nums 数组
+ * 最长递增子序列
+ * https://leetcode.cn/problems/longest-increasing-subsequence/
+ */
+function lengthOfLIS(nums: number[]): number {
+  const len = nums.length;
+  // 以 nums[i] 结尾 的「上升子序列」的长度
+  const dp = new Array(len).fill(1);
+  for (let i = 1; i < len; i++) {
+    for (let j = 0; j < i; j++) {
+      if (nums[i] > nums[j]) {
+        dp[i] = Math.max(dp[i], dp[j] + 1);
+      }
+    }
+  }
+  return Math.max(...dp);
+}
+
+export {};
