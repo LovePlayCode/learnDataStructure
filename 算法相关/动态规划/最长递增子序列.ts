08dd0c9a7e8f46c7a365be435e613d7cba600e05
lhl <1244224501@qq.com>
2025-03-28 21:47:29 +0800

    feat: commit

--- 算法相关/动态规划/最长递增子序列.ts
+++ 算法相关/动态规划/最长递增子序列.ts
@@ -6,6 +6,7 @@
 function lengthOfLIS(nums: number[]): number {
   const len = nums.length;
   // 以 nums[i] 结尾 的「上升子序列」的长度
+  // 周五水
   const dp = new Array(len).fill(1);
   for (let i = 1; i < len; i++) {
     for (let j = 0; j < i; j++) {
