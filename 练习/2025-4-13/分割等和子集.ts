d577999a73db43c644d71135bc9a9293236f240d
lhl <1244224501@qq.com>
2025-04-13 15:35:24 +0800

    feat: add file

---
+++ 练习/2025-4-13/分割等和子集.ts
@@ -0,0 +1,8 @@
+/**
+ * leetcode: https://leetcode.cn/problems/partition-equal-subset-sum/description/
+ * 题解: https://leetcode.cn/problems/partition-equal-subset-sum/solutions/13059/0-1-bei-bao-wen-ti-xiang-jie-zhen-dui-ben-ti-de-yo/
+ * @param nums 接收的数组
+ */
+function canPartition(nums: number[]): boolean {}
+
+export {};
