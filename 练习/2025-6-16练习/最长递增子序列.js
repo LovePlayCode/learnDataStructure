6817e22d6a40db828f0ec668bcc5c6bb13a3d09b
lhl <1244224501@qq.com>
2025-06-16 16:53:52 +0800

    feat: 最长递增子序列

---
+++ 练习/2025-6-16练习/最长递增子序列.js
@@ -0,0 +1,20 @@
+function lengthOfLIS(nums) {
+  const len = nums.length;
+  const memo = new Array(len).fill(0);
+  const dfs = (length) => {
+    if (memo[length] > 0) {
+      return memo[length];
+    }
+    for (let j = 0; j < length; j++) {
+      if (nums[j] < nums[length]) {
+        memo[length] = Math.max(memo[length], dfs(j));
+      }
+    }
+    return ++memo[length];
+  };
+  let ans = 0;
+  for (let i = 0; i < len; i++) {
+    ans = Math.max(ans, dfs(i));
+  }
+  return ans;
+}
