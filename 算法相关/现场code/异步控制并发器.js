2746ef65d22f645866fee095cd64583edd9c7576
lhl <1244224501@qq.com>
2025-06-22 17:32:51 +0800

    feat: complete async Concurrency Schedule

--- 算法相关/现场code/异步控制并发器.js
+++ 算法相关/现场code/异步控制并发器.js
@@ -7,16 +7,51 @@ function fetch(api) {
 }
 
 class ConcurrencyRequest {
+  // 保存所有待执行的任务
   arr = [];
+  // 跟踪正在执行的请求数量
   count = 0;
   constructor(size) {
     this.size = size;
   }
 
-  // 做异步的并发控制
-  request(api) {}
-}
+  // 当调用 request() 时，创建一个任务并加入队列
+  // 检查当前运行任务数是否小于最大并发数
+  // 如果未达上限，立即执行任务；否则等待
+  // 任务完成后自动触发下一个任务的执行
+  request(api) {
+    return new Promise((res, rej) => {
+      // 构造一个任务函数，执行真正的请求，如果此次请求完美执行，再去启动下一轮的任务。
+      const taskFet = async () => {
+        this.count++;
+        try {
+          const result = await fetch(api);
+          res(result);
+        } catch (error) {
+          rej(error);
+        } finally {
+          this.count--;
+          this.executeNext();
+        }
+      };
 
+      this.arr.push(taskFet);
+      this.executeNext();
+    });
+  }
+  executeNext() {
+    if (this.count < this.size && this.arr.length) {
+      const nextTask = this.arr.shift();
+      nextTask();
+    }
+  }
+  task(api, resolve) {
+    return fetch(api).then((res) => {
+      resolve(res);
+    });
+  }
+}
+const myFetch = new ConcurrencyRequest(2);
 myFetch.request("api1").then((res) => {
   console.log(res);
 });
