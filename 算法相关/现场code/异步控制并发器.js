bcf7fe856e4a283667d32306f26e45f8b92e4af9
lhl <1244224501@qq.com>
2025-06-22 17:08:05 +0800

    feat: complete templete

--- 算法相关/现场code/异步控制并发器.js
+++ 算法相关/现场code/异步控制并发器.js
@@ -1,3 +1,11 @@
+function fetch(api) {
+  return new Promise((res) => {
+    setTimeout(() => {
+      res(api);
+    }, Math.random() * 1000);
+  });
+}
+
 class ConcurrencyRequest {
   arr = [];
   count = 0;
@@ -5,5 +13,22 @@ class ConcurrencyRequest {
     this.size = size;
   }
 
+  // 做异步的并发控制
   request(api) {}
 }
+
+myFetch.request("api1").then((res) => {
+  console.log(res);
+});
+myFetch.request("api2").then((res) => {
+  console.log(res);
+});
+myFetch.request("api3").then((res) => {
+  console.log(res);
+});
+myFetch.request("api5").then((res) => {
+  console.log(res);
+});
+myFetch.request("api6").then((res) => {
+  console.log(res);
+});
