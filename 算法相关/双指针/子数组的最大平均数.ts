7379c44700ed81b7ce3e9dbdb8382fe637542c65
lhl <1244224501@qq.com>
2025-06-23 22:56:45 +0800

    feat: add Comment

--- 算法相关/双指针/子数组的最大平均数.ts
+++ 算法相关/双指针/子数组的最大平均数.ts
@@ -1,3 +1,13 @@
+/**
+ * leetcode: https://leetcode.cn/problems/maximum-average-subarray-i/
+ * @param nums
+ * @param k
+ * @returns
+ * 套路
+ * 1. 入
+ * 2. 更新
+ * 3. 出 重置状态
+ */
 function findMaxAverage(nums: number[], k: number): number {
   const len = nums.length;
   let sum = 0;
