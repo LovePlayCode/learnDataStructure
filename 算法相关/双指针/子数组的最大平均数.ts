0c7b71b6083a190ac60ab23fa64a8a50a8bf0d02
lhl <1244224501@qq.com>
2025-06-23 22:51:29 +0800

    feat: subarray maximum average

---
+++ 算法相关/双指针/子数组的最大平均数.ts
@@ -0,0 +1,19 @@
+function findMaxAverage(nums: number[], k: number): number {
+  const len = nums.length;
+  let sum = 0;
+  let ans = Number.MIN_SAFE_INTEGER;
+  for (let left = 0; left < len; left++) {
+    sum = sum + nums[left];
+
+    if (left < k - 1) {
+      continue;
+    }
+    console.log("sum==", sum, sum / 4);
+    ans = Math.max(ans, sum / k);
+    const out = left - k + 1;
+    sum = sum - nums[out];
+  }
+  return ans;
+}
+
+export {};
